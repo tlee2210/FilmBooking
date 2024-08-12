dcc191c348b2894f37c4d12abea770a194e321a9
monterdv <quangdaivi2303@gmail.com>
2024-08-13 00:26:35 +0700

    fix voucher task

--- backendApp/src/main/java/com/cinemas/job/VoucherTasks.java
+++ backendApp/src/main/java/com/cinemas/job/VoucherTasks.java
@@ -3,16 +3,35 @@ package com.cinemas.job;
 import com.cinemas.entities.Voucher;
 import com.cinemas.enums.StatusVoucher;
 import com.cinemas.repositories.VoucherRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Component
 public class VoucherTasks {
-      @Autowired
+    @Autowired
     private VoucherRepository voucherRepository;
 
-   @Scheduled(cron = "*/10 * * * * *")
+    @Scheduled(cron = "*/10 * * * * *")
     public void checkVoucher() {
-       List<Voucher> vouchersActive = voucherRepository.findByExpirationDateBefore(StatusVoucher.ACTIVE);
-       vouchersActive.forech
-
-   }
+        List<Voucher> vouchers = voucherRepository.findAll();
+        vouchers.forEach(voucher -> {
+            if (voucher.getStatusVoucher() != StatusVoucher.USEDUP) {
+                if (voucher.getExpirationDate().isBefore(LocalDate.now())) {
+                    if (voucher.getStatusVoucher() != StatusVoucher.EXPIRED) {
+                        voucher.setStatusVoucher(StatusVoucher.EXPIRED);
+                        voucherRepository.save(voucher);
+                    }
+                } else {
+                    if (voucher.getStatusVoucher() != StatusVoucher.ACTIVE) {
+                        voucher.setStatusVoucher(StatusVoucher.ACTIVE);
+                        voucherRepository.save(voucher);
+                    }
+                }
+            }
+        });
+    }
 }
